54bf54fb8d2829dbd9b2cbab7a1d4cdd26e306c8
Daniel Popoola <iamuchihadaniel236@gmail.com>
2024-10-11 07:28:38 +0100

    defined url patterns functions for habits

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Habit
 
-# Create your views here.
+@login_required
+def dashboard(request):
+    user_habits = Habit.objects.filter(user=request.user)
+    context = {
+        'habits':user_habits
+    }
+    return render(request, 'habits/dashboard.html', context)
+
+@login_required
+def create_habit(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        description = request.POST.get('description', '')
+        periodicity = request.POST['periodicity']
+        start_date = request.POST['start_date']
+
+        habit = Habit.objects.create(
+            user = request.user,
+            name=name,
+            description=description,
+            periodicity=periodicity,
+            last_completed=None,
+            streak=0
+        )
+
+        return redirect('habits/dashboard.html')
+    
+    return render(request, 'create_habit')
