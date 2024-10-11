8d3bb645f7cca0ba628b4f40488d1a55870cb1cb
Daniel Popoola <iamuchihadaniel236@gmail.com>
2024-10-11 10:15:45 +0100

    added delete_habit url app url

--- habits/urls.py
+++ habits/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('delete_habit/<int:habit_id>/', views.delete_habit, name='delete_habit'),
     path('create_habit', views.create_habit, name='create_habit'),
     path('dashboard', views.dashboard, name='dashboard'),
 ]
