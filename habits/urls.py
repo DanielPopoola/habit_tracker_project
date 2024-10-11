b8355489edb83a35013102f65ad6a44614f6ac4c
Daniel Popoola <iamuchihadaniel236@gmail.com>
2024-10-11 11:01:22 +0100

    added habit_analysis url to app url

--- habits/urls.py
+++ habits/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('habit_analysis', views.habit_analysis, name='habit_analysis'),
     path('delete_habit/<int:habit_id>/', views.delete_habit, name='delete_habit'),
     path('create_habit', views.create_habit, name='create_habit'),
     path('dashboard', views.dashboard, name='dashboard'),
