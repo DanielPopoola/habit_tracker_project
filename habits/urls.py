82a3ba6332b643c8b1504fc91bb101f2a0c83d7e
Daniel Popoola <iamuchihadaniel236@gmail.com>
2024-10-11 07:51:49 +0100

    corrected name error in create_habit url pattern

--- habits/urls.py
+++ habits/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create_habit', views.create_habit, name='dashboard'),
+    path('create_habit', views.create_habit, name='create_habit'),
     path('dashboard', views.dashboard, name='dashboard'),
 ]
